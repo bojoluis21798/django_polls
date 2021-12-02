09b7fb1d2bb10a984ac5b41d8bb6c3e8a889ab01
bojoluis21798 <bojoluis21798@gmail.com>
2021-12-02 11:33:21 +0800

    Use generic views

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,27 @@
 from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 # Create your views here.
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "lastest_question_list"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -40,8 +42,3 @@ def vote(request, question_id):
 
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
 
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
